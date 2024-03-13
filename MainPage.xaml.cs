8a47084b4fe1863ec8d49db2ef18b4611126344f
Prathyush Taneti <prathyush@iqdynamics-tech.com>
2024-03-13 05:59:51 +0530

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace CustomControls
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string username = this.Username.Text;
+                string password = this.Password.Text;
+                string displayText = "Logging In";
+                SemanticScreenReader.Default.Announce(displayText);
+                string result = await DisplayPromptAsync("Question 1", "What's your name?");
+                await DisplayAlert($"{username}", $"{password}", "Okay");
+                string resultUtil = (result.Length > 0) ? result : "Empty Input";
+                await DisplayAlert($"{resultUtil}", $"{resultUtil}", "Okay");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert($"{ex.Message}", $"{ex.StackTrace}", "Okay");
+            }
+        }
+    }
+
+}
