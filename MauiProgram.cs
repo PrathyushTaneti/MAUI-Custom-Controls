b5f32d28235c1eb1725cb00f22a928477a11a3aa
Prathyush Taneti <prathyush@iqdynamics-tech.com>
2024-03-15 16:48:47 +0530

    .

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace CustomControls
 {
@@ -9,6 +10,8 @@ namespace CustomControls
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiMaps()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
